1f3a3c41f44a15aedaff2ebb38ea8e17dddb8dc2
LEO22555 <lemartinezba@unal.edu.co>
2023-06-07 18:46:59 -0500

    sign up

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import SignIn from "./components/SignIn";
+// import ForgotPassword from "./components/ForgotPassword";
+// import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
     <div className="container">
-      <SignIn />
+      {/* <SignIn /> */}
+      {/* <ForgotPassword /> */}
+      <SignUp />
     </div>
   );
 }
